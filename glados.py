8b1c0075cfa5e8d04ff1d854b406e5febd0b9648
checkinall <146333034+checkinall@users.noreply.github.com>
2023-10-18 11:58:43 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -30,8 +30,8 @@ if __name__ == '__main__':
         email = state.json()['data']['email']
         if 'message' in checkin.text:
             mess = checkin.json()['message']
-            print(email+'----结果--'+mess)  # 日志输出
-            sendContent += email+'----'+mess+'\n'
+            print(email+' --= 结果 =--'+mess)  # 日志输出
+            sendContent += email+' - '+mess+'\n'
         else:
             requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效')
             print('cookie已失效')  # 日志输出
